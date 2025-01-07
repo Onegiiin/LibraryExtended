bdc2908d2bd0fad74b82c1d0bd5fc1ba629bac3b
Andrey <andrejkapinskij@gmail.com>
2025-01-07 03:10:58 +0300

    Initial commit

---
+++ src/main/java/by/library/controllers/LibraryController.java
@@ -0,0 +1,15 @@
+package by.library.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/library")
+public class LibraryController {
+
+    @GetMapping()
+    public String home(){
+        return "library/home";
+    }
+}
